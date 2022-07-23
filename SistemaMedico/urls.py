6ab91697bd9bb2bcc7c25770409004fa3fdebf67
karlo <ktorres@unsa.edu.pe>
2022-07-22 23:44:37 -0500

    creo la app Sistema y la base de datos, sin relaciones aun

--- SistemaMedico/urls.py
+++ SistemaMedico/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from login.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', MyHomeView, name="pag inicio"), 
 ]
--- login/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def MyHomeView(*args, **kwargs):
-    return HttpResponse('<h1>  hola mundo </h1>')
