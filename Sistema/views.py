a866acd67ecb5af4c95811dd8baceb45320f12b3
vindictaBit <83889542+vindictaBit@users.noreply.github.com>
2022-08-10 10:20:12 -0500

    Delete views.py

--- Sistema/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,redirect
-from django.views.generic import ListView
-from .models import *
-
-def loginView(request):
-    return render(request,"login.html")
-
-def HomeView(request):
-    return render(request,"home.html")
